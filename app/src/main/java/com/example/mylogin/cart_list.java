4272d405b49fa622eda216f5e8b23e4a09d88184
chaeyoon20 <lcu1027@gmail.com>
2021-05-04 13:26:06 +0900

    0504_3 cart_list.java modified

--- app/src/main/java/com/example/mylogin/cart_list.java
+++ app/src/main/java/com/example/mylogin/cart_list.java
@@ -6,6 +6,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 import java.lang.reflect.Array;
@@ -15,47 +19,64 @@ import java.util.List;
 
 public class cart_list extends AppCompatActivity {
 
-
     private Adapter_cart adapter;
     ItemTouchHelper helper;
     int count1 = 0;
     int count2 = 0;
 
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.cart_list);
-        init();
-
-        getData();
-
-
-    /*    p1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                count1++;
-                tv1.setText("" + count1);
-            }
-        });
-        m1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                count1--;
-                tv1.setText("" + count1);
-            }
-        });
-        p2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                count2++;
-                tv2.setText("" + count2);
-            }
-        });
-        m2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                count2--;
-                tv2.setText("" + count2);
-            }
-        });*/
-
-    }
+//    ImageButton btn_count_up, btn_count_down, btn_set_up, btn_set_down;
+//    TextView textView_count, textView_set;
+//    int count = 0;
+//    int set = 0;
+//
+//    protected void onCreate(Bundle savedInstanceState) {
+//
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.cart_list);
+//        init();
+//
+//        getData();
+//
+//        textView_count = findViewById(R.id.textView_count);
+//        textView_set = findViewById(R.id.textView_set);
+//
+//        btn_count_up = findViewById(R.id.btn_count_up);
+//        btn_count_up.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                count = count + 1;
+//                textView_count.setText(Integer.toString(count));
+//            }
+//        });
+//        btn_count_down = findViewById(R.id.btn_count_down);
+//        btn_count_down.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (count > 0) {
+//                    count = count - 1;
+//                    textView_count.setText(Integer.toString(count));
+//                }
+//            }
+//        });
+//        btn_set_up = findViewById(R.id.btn_set_up);
+//        btn_set_up.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                set = set + 1;
+//                textView_set.setText(Integer.toString(set));
+//            }
+//        });
+//        btn_set_down = findViewById(R.id.btn_set_down);
+//        btn_set_down.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (set > 0) {
+//                    set = set - 1;
+//                    textView_set.setText(Integer.toString(set));
+//                }
+//            }
+//        });
+//    }
 
     private void init() {
 
