2681cf420ca9ca6d73dca96678d0cd539f03dff4
SuBin-Park1113 <wlqkr23@naver.com>
2021-05-09 22:38:54 +0900

    0509

--- app/src/main/java/com/example/mylogin/Adapter_cart.java
+++ app/src/main/java/com/example/mylogin/Adapter_cart.java
@@ -1,8 +1,10 @@
 package com.example.mylogin;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,11 +13,15 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
 import java.util.List;
 
 public class Adapter_cart extends RecyclerView.Adapter<Adapter_cart.ItemViewHolder> implements ItemTouchHelperListener {
     // adapter에 들어갈 list 입니다.
     private ArrayList<Data> ex_list = new ArrayList<>();
+    private ArrayList<Dictionary> mList;
+    private OnItemClickListener mListener = null;
+    int count=0, set=0;
 
     @NonNull
     @Override
@@ -53,24 +59,98 @@ public class Adapter_cart extends RecyclerView.Adapter<Adapter_cart.ItemViewHold
         ex_list.remove(position); notifyItemRemoved(position);
     }
 
+    public interface OnItemClickListener{
+        void onItemClick(View v, int position);
+    }
+
+    // OnItemClickListener 리스너 객체 참조를 어댑터에 전달하는 메서드
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.mListener = listener ;
+    }
+
     // RecyclerView의 핵심인 ViewHolder 입니다.
     // 여기서 subView를 setting 해줍니다.
     class ItemViewHolder extends RecyclerView.ViewHolder {
 
         public ImageView imageView;
         public TextView index;
+        public TextView textView_count, textView_set;
+        public ImageButton btn_count_up, btn_count_down, btn_set_up, btn_set_down;
+        /*public TextView textView_count, textView_set;
+        int count = 0;
+        int set = 0;*/
 
         ItemViewHolder(View itemView) {
             super(itemView);
 
             index = itemView.findViewById(R.id.index);
             imageView = itemView.findViewById(R.id.image);
-        }
+            textView_count = itemView.findViewById(R.id.textView_count);
+            textView_set = itemView.findViewById(R.id.textView_set);
+            btn_count_down=itemView.findViewById(R.id.btn_count_down);
+            btn_count_up=itemView.findViewById(R.id.btn_count_up);
+            btn_set_down=itemView.findViewById(R.id.btn_set_down);
+            btn_set_up=itemView.findViewById(R.id.btn_set_up);
 
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    if(position!=RecyclerView.NO_POSITION){
+                        if(mListener!=null){
+                            mListener.onItemClick(v,position);
+                        }
+                    }
+                }
+            });
+
+            /*
+            //횟수 및 세트 조절
+            textView_count = itemView.findViewById(R.id.textView_count);
+            textView_set = itemView.findViewById(R.id.textView_set);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    if(position!=RecyclerView.NO_POSITION){
+                        if(mListener!=null){
+                            mListener.onItemClick(v,position);
+                        }
+                    }
+                }
+            });*/
+
+        }
 
         void onBind(Data data) {
             index.setText(Integer.toString(data.getIndex()));
             imageView.setImageResource(data.getResId());
+            //textView_count.setText(Integer.toString(data.getIndex()));
+            //textView_set.setText(Integer.toString(data.getIndex()));
         }
+
+        /*public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_count_down:
+                    count--;
+                    textView_count.setText(Integer.toString(count));
+                    break;
+                case R.id.btn_count_up:
+                    textView_count.setText(Integer.toString(count));
+                    count++;
+                    break;
+                case R.id.btn_set_down:
+                    textView_count.setText(Integer.toString(set));
+                    set--;
+                    break;
+                case R.id.btn_set_up:
+                    textView_count.setText(Integer.toString(set));
+                    set++;
+                    break;
+            }
+        }*/
     }
 }
--- app/src/main/java/com/example/mylogin/cart_list.java
+++ app/src/main/java/com/example/mylogin/cart_list.java
@@ -5,10 +5,12 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -20,6 +22,13 @@ import java.util.List;
 public class cart_list extends AppCompatActivity {
 
     private Adapter_cart adapter;
+    private Adapter_cart count_up_adapter;
+    private Adapter_cart count_down_adapter;
+    private Adapter_cart set_up_adapter;
+    private Adapter_cart set_down_adapter;
+    private Adapter_cart textCount_adapter;
+    private Adapter_cart textSet_adapter;
+
     ItemTouchHelper helper;
     int count1 = 0;
     int count2 = 0;
@@ -37,45 +46,120 @@ public class cart_list extends AppCompatActivity {
 
         getData();
 
-//        textView_count = findViewById(R.id.textView_count);
-//        textView_set = findViewById(R.id.textView_set);
-//
-//        btn_count_up = findViewById(R.id.btn_count_up);
-//        btn_count_up.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                count = count + 1;
-//                textView_count.setText(Integer.toString(count));
-//            }
-//        });
-//        btn_count_down = findViewById(R.id.btn_count_down);
-//        btn_count_down.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (count > 0) {
-//                    count = count - 1;
-//                    textView_count.setText(Integer.toString(count));
-//                }
-//            }
-//        });
-//        btn_set_up = findViewById(R.id.btn_set_up);
-//        btn_set_up.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                set = set + 1;
-//                textView_set.setText(Integer.toString(set));
-//            }
-//        });
-//        btn_set_down = findViewById(R.id.btn_set_down);
-//        btn_set_down.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (set > 0) {
-//                    set = set - 1;
-//                    textView_set.setText(Integer.toString(set));
-//                }
-//            }
-//        });
+        //운동시작 누르면 Exercise 로
+        ImageView start = (ImageView) findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(cart_list.this, Exercise.class);
+                startActivity(intent);
+            }
+        });
+
+        //하단바
+        ImageView home = (ImageView) findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(cart_list.this, home.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView scale = (ImageView) findViewById(R.id.scale);
+        scale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(cart_list.this, BodyProfile.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView memo = (ImageView) findViewById(R.id.calendar);
+        memo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(cart_list.this, Calendar.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView mypage = (ImageView) findViewById(R.id.mypage);
+        mypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(cart_list.this, Mypage.class);
+                startActivity(intent);
+            }
+        });
+
+        //횟수 및 세트 조절
+        adapter.setOnItemClickListener(new Adapter_cart.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                if(v.getId()==R.id.btn_count_down)
+                {
+                    count--;
+                    textView_count.setText(Integer.toString(count));
+                }
+                else if(v.getId()==R.id.btn_count_up)
+                {
+                    count++;
+                    textView_count.setText(Integer.toString(count));
+                }
+                else if(v.getId()==R.id.btn_set_down)
+                {
+                    set++;
+                    textView_set.setText(Integer.toString(set));
+                }
+                else if(v.getId()==R.id.btn_set_up)
+                {
+                    set++;
+                    textView_count.setText(Integer.toString(set));
+                }
+            }
+        });
+
+        /*//횟수 및 세트 조절
+        textView_count = findViewById(R.id.textView_count);
+        textView_set = findViewById(R.id.textView_set);
+
+        btn_count_up = findViewById(R.id.btn_count_up);
+        btn_count_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count = count + 1;
+                textView_count.setText(Integer.toString(count));
+            }
+        });
+        btn_count_down = findViewById(R.id.btn_count_down);
+        btn_count_down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (count > 0) {
+                    count = count - 1;
+                    textView_count.setText(Integer.toString(count));
+                }
+            }
+        });
+        btn_set_up = findViewById(R.id.btn_set_up);
+        btn_set_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                set = set + 1;
+                textView_set.setText(Integer.toString(set));
+            }
+        });
+        btn_set_down = findViewById(R.id.btn_set_down);
+        btn_set_down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (set > 0) {
+                    set = set - 1;
+                    textView_set.setText(Integer.toString(set));
+                }
+            }
+        });*/
     }
 
     private void init() {
@@ -103,6 +187,8 @@ public class cart_list extends AppCompatActivity {
             // 각 값이 들어간 data를 adapter에 추가합니다.
             adapter.addItem(data);
         }
+
+
 
         // adapter의 값이 변경되었다는 것을 알려줍니다.
         adapter.notifyDataSetChanged();
