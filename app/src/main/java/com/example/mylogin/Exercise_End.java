3a83bd3678435045af2e8a8a875ab7b8d1fd5d9b
miiiin0 <79246888+miiiin0@users.noreply.github.com>
2021-05-29 15:05:34 +0900

    drawable 추가

---
+++ app/src/main/java/com/example/mylogin/Exercise_End.java
@@ -0,0 +1,14 @@
+package com.example.mylogin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Exercise_End extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exercise_end);
+    }
+}
